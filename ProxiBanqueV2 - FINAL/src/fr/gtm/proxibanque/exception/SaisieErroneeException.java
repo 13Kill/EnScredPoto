873dfabbd54eda45378d19f0a9b5e25e448d21fd
13Kill <hedi.soumri@gmail.com>
2015-12-15 22:35:16 +0100

    LES COMMENTAIRES C'EST BIEN OK ?

---
+++ ProxiBanqueV2 - FINAL/src/fr/gtm/proxibanque/exception/SaisieErroneeException.java
@@ -0,0 +1,13 @@
+package fr.gtm.proxibanque.exception;
+
+public class SaisieErroneeException extends Exception {
+	private static final long serialVersionUID = 4181298640255186938L;
+
+	public SaisieErroneeException() {
+		super();
+	}
+	public SaisieErroneeException(String s) {
+		super(s);
+	}
+}
+
---
+++ ProxiBanqueV2 - FINAL/src/fr/gtm/proxibanque/util/Bdd.java
@@ -0,0 +1,40 @@
+package fr.gtm.proxibanque.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+public class Bdd {
+	
+	//pilote:type de Driver==>aller ds propriete de connection ds Service
+	private static String pilote = "oracle.jdbc.OracleDriver";
+	//le nom du Driver==>reprendre nom ds propriete de connection ds Service
+	private static String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	//login et password du Database User
+	private static String utilisateur = "admin";
+	private static String password = "admin";
+
+	public static Connection seConnecter() {
+	    Connection cnx = null;
+	        try {
+	            Class.forName(pilote);
+	            cnx = DriverManager.getConnection(url, utilisateur, password);
+
+	        } catch (ClassNotFoundException | SQLException ex) {
+	            Logger.getLogger(Bdd.class.getName()).log(Level.SEVERE, null, ex);
+	        }
+	        return cnx;
+	    }
+
+	    public static void seDeconnecter(Connection cnx) {
+
+	        try {
+	            cnx.close();
+	        } catch (SQLException ex) {
+	            Logger.getLogger(Bdd.class.getName()).log(Level.SEVERE, null, ex);
+	        }
+	    }
+
+}
